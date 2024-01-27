fb632d6de9a2848b147aa688f10729d5a118f3d0
unknown <sohailshahidi1435@gmail.com>
2024-01-27 18:16:03 +0430

    Add getCaloriesFromInputs function

--- JavaScript/Form Validation/script.js
+++ JavaScript/Form Validation/script.js
@@ -31,3 +31,16 @@ function addEntry() {
   />`;
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    const currVal = cleanInputString(list[i].value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+
+    }
+  }
+}
