c2138f50f887b4f9ba9873a4d613f0461aa158b4
unknown <sohailshahidi1435@gmail.com>
2024-01-28 22:06:33 +0430

    Update getCaloriesFromInputs function

--- JavaScript/Form Validation/script.js
+++ JavaScript/Form Validation/script.js
@@ -40,7 +40,11 @@ function getCaloriesFromInputs(list) {
     const invalidInputMatch = isInvalidInput(currVal);
 
     if (invalidInputMatch) {
-
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
     }
+    calories += Number(currVal);
   }
+  return calories;
 }
