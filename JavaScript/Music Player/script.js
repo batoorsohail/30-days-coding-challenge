24d2a56f0557a44a945d3594929b50f803040de7
unknown <sohailshahidi1435@gmail.com>
2024-02-02 12:00:34 +0430

    Sort songs by alphabetical order

--- JavaScript/Music Player/script.js
+++ JavaScript/Music Player/script.js
@@ -107,4 +107,16 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songsHTML;
   }
 
+userData?.songs.sort((a,b) => {
+  if (a.title < b.title) {
+    return -1;
+  }
+
+  if (a.title > b.title) {
+    return 1;
+  }
+
+  return 0;
+});
+
   renderSongs(userData?.songs);
