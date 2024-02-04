775fdcf16a9bd622dd2e0cbb4b3d236b2b49165c
unknown <sohailshahidi1435@gmail.com>
2024-02-04 14:55:39 +0430

    Add getCurrentSongIndex function

--- JavaScript/Music Player/script.js
+++ JavaScript/Music Player/script.js
@@ -130,6 +130,8 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songsHTML;
   }
 
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+
 playButton.addEventListener("click", () => {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
