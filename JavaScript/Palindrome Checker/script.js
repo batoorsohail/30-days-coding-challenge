8c85364395c1290470f9fa692850608c3e55c858
unknown <sohailshahidi1435@gmail.com>
2024-02-07 20:40:25 +0430

    Add EventListener to checkButtong

--- JavaScript/Palindrome Checker/script.js
+++ JavaScript/Palindrome Checker/script.js
@@ -6,3 +6,17 @@ const isPalindrome = (str) => {
   str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
   return str === str.split('').reverse().join('');
 }
+
+checkButton.addEventListener("click", () => {
+  const inputValue = textInput.value.trim();
+
+  if (inputValue === "") {
+    alert("Please input a value")
+  } else {
+    if (isPalindrome(inputValue)) {
+      result.textContent = `${inputValue} is a Palindrome`;
+    } else {
+      result.textContent = `${inputValue} is not a Palindrome`;
+    }
+  }
+});
