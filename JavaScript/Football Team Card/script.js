037ab498cbd2d429614045621237cc8000d60997
unknown <sohailshahidi1435@gmail.com>
2024-02-08 16:22:53 +0430

    Add eventListener to playersDropdownList

--- JavaScript/Football Team Card/script.js
+++ JavaScript/Football Team Card/script.js
@@ -195,3 +195,13 @@ const setPlayerCards = (arr = players) => {
     )
     .join("");
 };
+
+playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+  
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+  }
+});
