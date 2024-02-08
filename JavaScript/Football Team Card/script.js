9389de2621bddbaf2096f53c099cdb438f8a405d
unknown <sohailshahidi1435@gmail.com>
2024-02-08 14:29:34 +0430

    Display head data

--- JavaScript/Football Team Card/script.js
+++ JavaScript/Football Team Card/script.js
@@ -174,3 +174,8 @@ const myFavoriteFootballTeam = {
 Object.freeze(myFavoriteFootballTeam);
 const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
+
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
