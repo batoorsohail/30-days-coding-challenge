960cb43a7a74f90a526ac925ddf5a55b871458ea
unknown <sohailshahidi1435@gmail.com>
2024-02-10 15:51:36 +0430

    Display tasks

--- JavaScript/Todo List/script.js
+++ JavaScript/Todo List/script.js
@@ -28,6 +28,7 @@ discardBtn.addEventListener("click", () => {
   taskForm.classList.toggle("hidden");
 });
 
+
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -39,7 +40,21 @@ taskForm.addEventListener("submit", (e) => {
     description: descriptionInput.value,
   };
 
-  if (dataArrIndex === -1) {
+   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
+
+  taskData.forEach(({id, title, date, description}) => {
+      (tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+        </div>
+      `)
+    }
+  );
+  taskForm.classList.toggle("hidden");
 });
