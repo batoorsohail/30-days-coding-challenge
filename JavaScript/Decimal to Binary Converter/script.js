30d088e88f7366c99aaeac43fd1cf59b700bfa45
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-13 16:40:46 +0430

    Add checkUserInput function

--- JavaScript/Decimal to Binary Converter/script.js
+++ JavaScript/Decimal to Binary Converter/script.js
@@ -1,3 +1,10 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+const checkUserInput = () => {
+
+
+  console.log(numberInput.value);
+};
+
