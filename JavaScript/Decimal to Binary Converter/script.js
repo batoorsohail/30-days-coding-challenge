7f2ae1619f4476a7f91bbd155de523998818aa8f
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-14 14:06:16 +0430

    Update decimalToBinary function

--- JavaScript/Decimal to Binary Converter/script.js
+++ JavaScript/Decimal to Binary Converter/script.js
@@ -3,6 +3,24 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
   
 };
 
