342a1e354f0c6310dc9309fd837e7054aac62edf
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-15 22:49:53 +0430

    Update files

--- JavaScript/Decimal to Binary Converter/script.js
+++ JavaScript/Decimal to Binary Converter/script.js
@@ -2,31 +2,8 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countdown = (number) => {
-  console.log(number);
-
-  if (number === 0) {
-    return;
-  } else {
-    countdown(number - 1);
-  }
-};
-
-countdown(3);
-
 const decimalToBinary = (input) => {
-  let binary = "";
 
-  if (input === 0) {
-    binary = "0";
-  }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {
