bfe18867503e3d989a63ee77919083864428cbc8
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-16 09:54:23 +0430

    Fix decimalToBinary function bug

--- JavaScript/Decimal to Binary Converter/script.js
+++ JavaScript/Decimal to Binary Converter/script.js
@@ -3,12 +3,14 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  if (input === 0) {
-    return "";
+  if (input === 0 || input === 1) {
+    return String(input);
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
 };
+
+
 
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
@@ -16,8 +18,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
-  result.textContent = decimalToBinary()
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
