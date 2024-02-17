3160f24cbd3faf6acb0079fba7bf494fa03c857c
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-17 15:11:13 +0430

    Add displayAuthors function

--- JavaScript/Author Page/script.js
+++ JavaScript/Author Page/script.js
@@ -1,6 +1,10 @@
 const authorContainer = document.getElementById('author-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
 
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
 fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
   .then((res) => res.json())
   .then((data) => {
@@ -9,3 +13,7 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
   .catch((err) => {
     console.error(`There was an error: ${err}`);
   });
+
+const displayAuthors = (authors) => {
+
+};
