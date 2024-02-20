7afab165063c0477ee8667415fdf3ef05e400e9b
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-20 16:07:45 +0430

    Add styling

--- React Redux/react-redux/src/features/counter/Counter.jsx
+++ React Redux/react-redux/src/features/counter/Counter.jsx
@@ -14,18 +14,18 @@ const Counter = () => {
   }
 
   return (
-    <section className="flex flex-col items-center justify-center h-screen">
+    <section className="flex flex-col items-center justify-center gap-5 h-screen">
       <p>{count}</p>
       <div>
         <button className="px-5 py-2 rounded-md border" onClick={() => dispatch(increment())}>+</button>
         <button className="px-5 py-2 rounded-md border" onClick={() => dispatch(decrement())}>-</button>
       </div>
       <div>
-        <input type="text" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+        <input type="text" className="px-5 py-2 rounded-md border" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
       </div>
       <div>
-        <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
-        <button onClick={() => dispatch(resetAll())}>Reset</button>
+        <button className="px-5 py-2 rounded-md border" onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+        <button className="px-5 py-2 rounded-md border" onClick={() => dispatch(resetAll())}>Reset</button>
       </div>
     </section>
   )
