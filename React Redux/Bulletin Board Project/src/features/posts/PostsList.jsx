ae071be382acaaebedfec4114c9f1354d8ba8b3a
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-21 15:32:50 +0430

    Add posts styles

--- React Redux/Bulletin Board Project/src/features/posts/PostsList.jsx
+++ React Redux/Bulletin Board Project/src/features/posts/PostsList.jsx
@@ -5,15 +5,15 @@ const PostsList = () => {
   const posts = useSelector(selectAllPosts)
 
   const renderedPosts = posts.map(post => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
+    <article key={post.id} className="border border-white rounded-lg py-5 px-8">
+      <h3 className="text-2xl text-white font-bold font-serif">{post.title}</h3>
+      <p className="text-lg text-white">{post.content.substring(0, 100)}</p>
     </article>
   ))
 
   return (
-    <section>
-      <h2>Posts</h2>
+    <section className="flex flex-col items-center justify-center gap-5 h-screen bg-slate-800">
+      <h2 className="text-5xl text-white font-bold">Posts</h2>
       {renderedPosts}
     </section>
   )
