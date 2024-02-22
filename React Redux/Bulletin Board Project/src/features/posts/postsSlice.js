86ba40ce02a5f8d1d95e6a5ffeb4fe81f25e3e44
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-22 16:58:20 +0430

    Update postsSlice.js

--- React Redux/Bulletin Board Project/src/features/posts/postsSlice.js
+++ React Redux/Bulletin Board Project/src/features/posts/postsSlice.js
@@ -1,35 +1,11 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Whatever",
-    content: "Whatever you wanna write...",
-    date: sub(new Date(), {minutes: 10}).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    }
-  },
-  {
-    id: "2",
-    title: "Learning Redux",
-    content: "We are going to learn RTK...",
-    date: sub(new Date(),
-    {minutes: 5}).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    }
-  }
-]
+const initialState = {
+  posts: [],
+  status: "idle", // "idle" | "loading" | "succeed" | "failed"
+  error: null
+}
 
 const postsSlice = createSlice({
   name: "posts",
@@ -37,7 +13,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -60,7 +36,7 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const {postId, reaction} = action.payload;
-      const existingPost = state.find(post => post.id === postId);
+      const existingPost = state.posts.find(post => post.id === postId);
       if(existingPost) {
         existingPost.reactions[reaction]++
       }
@@ -68,7 +44,7 @@ const postsSlice = createSlice({
   }
 })
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
