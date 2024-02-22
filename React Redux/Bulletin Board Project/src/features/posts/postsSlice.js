7d5a66a6723f250fcb7d8ed573c3d58567f4e442
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-22 17:23:14 +0430

    Fetch posts from api

--- React Redux/Bulletin Board Project/src/features/posts/postsSlice.js
+++ React Redux/Bulletin Board Project/src/features/posts/postsSlice.js
@@ -1,11 +1,19 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
+import axios from "axios";
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const initialState = {
   posts: [],
   status: "idle", // "idle" | "loading" | "succeed" | "failed"
   error: null
 }
+
+const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await axios.get(POSTS_URL);
+  return response.data;
+})
 
 const postsSlice = createSlice({
   name: "posts",
@@ -41,6 +49,35 @@ const postsSlice = createSlice({
         existingPost.reactions[reaction]++
       }
     }
+  },
+  extraReducers(builder){
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "succeed";
+        // Adding date and reactions
+        let min = 1;
+        const loadedPosts = action.payload.map(post => {
+          post.date = sub(new Date(), {minutes: min++}).toISOString();
+          posts.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+          }
+          return post;
+        });
+
+        //Add any fetched posts to the array
+        state.posts = state.posts.concat(loadedPosts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
   }
 })
 
