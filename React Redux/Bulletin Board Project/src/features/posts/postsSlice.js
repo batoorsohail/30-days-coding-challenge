d46549cc72e2686e31546e65a07e26f8cbca5614
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-23 14:48:17 +0430

    Send posts to api

--- React Redux/Bulletin Board Project/src/features/posts/postsSlice.js
+++ React Redux/Bulletin Board Project/src/features/posts/postsSlice.js
@@ -10,8 +10,13 @@ const initialState = {
   error: null
 }
 
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   const response = await axios.get(POSTS_URL);
+  return response.data;
+})
+
+export const addNewPost = createAsyncThunk("posts/addNewPost", async (initialPost) => {
+  const response = await axios.post(POSTS_URL, initialPost);
   return response.data;
 })
 
@@ -77,6 +82,19 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        action.payload.userId = Number(action.payload.userId);
+        action.payload.date = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsUp: 0,
+          hooray: 0,
+          heart: 0,
+          rocket: 0,
+          eyes: 0,
+        };
+        console.log(action.payload);
+        state.posts.push(action.payload);
       })
   }
 })
