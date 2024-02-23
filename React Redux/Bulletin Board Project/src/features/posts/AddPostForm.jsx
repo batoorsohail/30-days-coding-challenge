a227754dc824c33d48fad25ecae31c3f09d6da33
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-23 14:53:43 +0430

    Refactor canSave variable

--- React Redux/Bulletin Board Project/src/features/posts/AddPostForm.jsx
+++ React Redux/Bulletin Board Project/src/features/posts/AddPostForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
@@ -10,12 +10,15 @@ const AddPostForm = () => {
   const [ title, setTitle ] = useState("");
   const [ content, setContent ] = useState("");
   const [ userId, setUserId ] = useState("");
+  const [ addRequestStatus, SetAddRequestStatus ] = useState("idle");
 
   const users = useSelector(selectAllUsers);
 
   const onTitleChanged = e => setTitle(e.target.value);
   const onContentChanged = e => setContent(e.target.value);
   const onAuthorChanged = e => setUserId(e.target.value);
+
+  const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
 
   const onSavePostClicked = () => {
     if (title && content) {
@@ -26,8 +29,6 @@ const AddPostForm = () => {
       setContent("");
     }
   }
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
   const userOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
