7c6807ffb1b150f99931d5e9129b0a699dceec4d
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-25 17:30:07 +0430

    add page header

--- React Redux/Blog Project/src/components/Layout.js
+++ React Redux/Blog Project/src/components/Layout.js
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 const Layout = () => {
   return (
-    <main className="app">
-      <Outlet />
-    </main>
+    <>
+      <Header />
+      <main className="app">
+        <Outlet />
+      </main>
+    </>
   )
 }
 
