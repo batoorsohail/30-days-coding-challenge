54166c63257c0ac416bb7c6920c9e65beb00e40e
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-02-29 17:14:19 +0430

    Fix SinglePostPage component bug

--- React Redux/Blog Project/src/features/posts/SinglePostPage.js
+++ React Redux/Blog Project/src/features/posts/SinglePostPage.js
@@ -25,7 +25,7 @@ const SinglePostPage = () => {
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <p className="postCredit">
-        <Link to={`post/edit/${post.id}`}>Edit Post</Link>
+        <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
         <PostAuthor userId={post.userId} />
         <TimeAgo date={post.date} />
       </p>
