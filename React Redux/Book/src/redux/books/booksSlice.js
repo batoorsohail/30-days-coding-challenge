70c8393766956ea2c65404b34ac985aa13df04cf
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-02 15:38:57 +0430

    Add booksSlice.js file

---
+++ React Redux/Book/src/redux/books/booksSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  books: []
+};
+
+const booksSlice = createSlice({
+  name: "books",
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      const book = action.payload;
+      state.books.push(book);
+    },
+    removeBook: (state, action) => {
+      const deleteBookId = action.payload;
+      state.books = state.books.filter((book) => book.id !== deleteBookId);
+    }
+  }
+})
+
+export const { addBook, removeBook }= booksSlice.actions;
+export default booksSlice.reducer;
