63905c657b6da4e31d7518bde88b045fe8deb42a
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-02 21:18:18 +0430

    Add Categories and Home pages

--- React Redux/Book/src/App.jsx
+++ React Redux/Book/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CategoriesPage from "./pages/CategoriesPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element='' />
-      <Route path="/" element='' />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<CategoriesPage />} />
     </Routes>
   );
 }
