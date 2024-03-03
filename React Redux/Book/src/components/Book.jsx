68bd0e62985276e976dad1da78a47183947c2aa3
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-03 15:22:11 +0430

    Add books style

--- React Redux/Book/src/components/Book.jsx
+++ React Redux/Book/src/components/Book.jsx
@@ -23,13 +23,13 @@ const Book = () => {
   ]
   
   return (
-    <section className="flex justify-center flex-wrap gap-40">
+    <section className="flex justify-center flex-wrap gap-40 pt-20">
       {booksData.map((book) => {
         return (
-          <div className="bg-white" key={book.id}>
+          <div className="bg-white py-5 px-10 rounded-md" key={book.id}>
             <p className="text-2xl">{book.name}</p>
             <p className="text-2xl">{book.author}</p>
-            <button type="button">Delete</button>
+            <button type="button" className="bg-red-600 text-white font-bold py-2 px-5 rounded-md">Delete</button>
           </div>
         )
       })}
