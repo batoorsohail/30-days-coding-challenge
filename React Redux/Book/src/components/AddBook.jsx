313358b8561229570f6d3657e734a6205033f6f0
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-03 19:13:30 +0430

    Update files

--- React Redux/Book/src/components/AddBook.jsx
+++ React Redux/Book/src/components/AddBook.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux"
 import { useState } from "react";
-import { addBook } from "../redux/books/booksSlice";
 import { nanoid } from "nanoid";
+import { addBook } from "../redux/books/booksSlice";
 
 const AddBook = () => {
   const dispatch = useDispatch();
@@ -12,12 +12,15 @@ const AddBook = () => {
   const onBookChanged = e => (setBookName(e.target.value));
   const onAuthorChanged = e => (setAuthorName(e.target.value));
 
-  const onAddBookClick = () => {
+  const onSubmitAddBook = (e) => {
+    e.preventDefault();
+
     const newBook = {
       item_id: nanoid(),
       title: bookName,
       author: authorName
     }
+    
     dispatch(addBook(newBook));
     setBookName("");
     setAuthorName("");
@@ -25,9 +28,11 @@ const AddBook = () => {
 
   return (
     <section className="flex justify-center gap-10 w-full mt-20">
-      <input type="text" placeholder="Book Name" onChange={onBookChanged} className="px-[6px] py-[4px]" required />
-      <input type="text" placeholder="Author name" onChange={onAuthorChanged} className="px-[6px] py-[4px]" required />
-      <button onClick={onAddBookClick} className="bg-blue-600 text-white font-bold px-5 py-[4px]">Add Book</button>
+      <form onSubmit={onSubmitAddBook}>
+        <input type="text" placeholder="Book Name" onChange={onBookChanged} className="px-[6px] py-[4px]" required />
+        <input type="text" placeholder="Author name" onChange={onAuthorChanged} className="px-[6px] py-[4px]" required />
+        <button className="bg-blue-600 text-white font-bold px-5 py-[4px]">Add Book</button>
+      </form>
     </section>
   )
 }
