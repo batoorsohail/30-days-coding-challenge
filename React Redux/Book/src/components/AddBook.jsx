d64a647ad2ecf53985fea574e3816cf319a908db
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-03 19:02:11 +0430

    Add functionality to add a new book

--- React Redux/Book/src/components/AddBook.jsx
+++ React Redux/Book/src/components/AddBook.jsx
@@ -1,9 +1,31 @@
+import { useDispatch } from "react-redux"
+import { useState } from "react";
+import { addBook } from "../redux/books/booksSlice";
+
 const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const [ bookName, setBookName ] = useState("");
+  const [ authorName, setAuthorName ] = useState("");
+
+  const onBookChanged = e => (setBookName(e.target.value));
+  const onAuthorChanged = e => (setAuthorName(e.target.value));
+
+  const onAddBookClick = () => {
+    const newBook = {
+      title: bookName,
+      author: authorName
+    }
+    dispatch(addBook(newBook));
+    setBookName("");
+    setAuthorName("");
+  }
+
   return (
     <section className="flex justify-center gap-10 w-full mt-20">
-      <input type="text" placeholder="Book Name" className="px-[6px] py-[4px]" required />
-      <input type="text" placeholder="Author name" className="px-[6px] py-[4px]" required />
-      <button className="bg-blue-600 text-white font-bold px-5 py-[4px]">Add Book</button>
+      <input type="text" placeholder="Book Name" onChange={onBookChanged} className="px-[6px] py-[4px]" required />
+      <input type="text" placeholder="Author name" onChange={onAuthorChanged} className="px-[6px] py-[4px]" required />
+      <button onClick={onAddBookClick} className="bg-blue-600 text-white font-bold px-5 py-[4px]">Add Book</button>
     </section>
   )
 }
--- React Redux/Book/src/components/Book.jsx
+++ React Redux/Book/src/components/Book.jsx
@@ -7,9 +7,9 @@ const Book = () => {
     <section className="flex justify-center flex-wrap gap-40 pt-20">
       {books.map((book) => {
         return (
-          <div className="bg-white py-5 px-10 rounded-md" key={book.item_id}>
-            <p className="text-2xl">{book.title}</p>
-            <p className="text-2xl">{book.author}</p>
+          <div className="flex flex-col bg-white py-5 px-10 rounded-md gap-5 w-1/5" key={book.item_id}>
+            <p className="text-2xl font-bold">{book.title}</p>
+            <p className="text-xl">{book.author}</p>
             <button type="button" className="bg-red-600 text-white font-bold py-2 px-5 rounded-md">Delete</button>
           </div>
         )
