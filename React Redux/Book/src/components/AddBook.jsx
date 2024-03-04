5b9ee1a248228ef1514501de78b5401ff370fb66
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-04 22:02:56 +0430

    Add category option

--- React Redux/Book/src/components/AddBook.jsx
+++ React Redux/Book/src/components/AddBook.jsx
@@ -8,9 +8,11 @@ const AddBook = () => {
 
   const [bookName, setBookName] = useState('');
   const [authorName, setAuthorName] = useState('');
+  const [bookCategory, setBookCategory] = useState('');
 
   const onBookChanged = (e) => (setBookName(e.target.value));
   const onAuthorChanged = (e) => (setAuthorName(e.target.value));
+  const onCategoryChanged = (e) => (setBookCategory(e.target.value))
 
   const onSubmitAddBook = (e) => {
     e.preventDefault();
@@ -19,11 +21,14 @@ const AddBook = () => {
       item_id: nanoid(),
       title: bookName,
       author: authorName,
+      category: bookCategory
     };
 
     dispatch(addBook(newBook));
+    
     setBookName('');
     setAuthorName('');
+    setBookCategory('');
   };
 
   return (
@@ -31,9 +36,13 @@ const AddBook = () => {
       <form onSubmit={onSubmitAddBook} className="flex justify-center gap-10 mt-20">
         <input type="text" placeholder="Book Name" onChange={onBookChanged} className="px-[6px] py-[4px]" required />
         <input type="text" placeholder="Author name" onChange={onAuthorChanged} className="px-[6px] py-[4px]" required />
+        <select name="category" onChange={onCategoryChanged}>
+          <option value="Fiction">Fiction</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Nonfiction">Nonfiction</option>
+          <option value="Drama">Drama</option>
+        </select>
         <button type="submit" className="bg-blue-600 text-white font-bold px-5 py-[4px]">Add Book</button>
-        {' '}
-
       </form>
     </section>
   );
--- React Redux/Book/src/components/Book.jsx
+++ React Redux/Book/src/components/Book.jsx
@@ -15,7 +15,8 @@ const Book = () => {
       {books.map((book) => (
         <div className="flex flex-col bg-white py-5 px-10 rounded-md gap-5 w-1/5" key={book.item_id}>
           <p className="text-2xl font-bold">{book.title}</p>
-          <p className="text-xl">{book.author}</p>
+          <p className="text-xl font-bold">{book.author}</p>
+          <p className="text-xl">{book.category}</p>
           <button type="button" onClick={() => onDeleteBook(book)} className="bg-red-600 text-white font-bold py-2 px-5 rounded-md">Delete</button>
         </div>
       ))}
