7cf53585c48b153a203989e1e21dcb51482f378a
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-10 22:11:56 +0430

    Add styling for RocketsPage.jsx file

--- React Redux/space/src/pages/RocketsPage.jsx
+++ React Redux/space/src/pages/RocketsPage.jsx
@@ -12,13 +12,16 @@ const RocketsPage = () => {
 
   const rockets = useSelector(selectAllRockets);
   return (
-    <section>
+    <section className="flex flex-col gap-10 px-20">
       {rockets.map((rocket) => (
-        <section key={rocket.id}>
-          <img src={rocket.flickerImage} alt="rocket" className="w-72" />
-          <p className="text-xl font-bold">{rocket.rocketName}</p>
-          <p>{rocket.description}</p>
-        </section>
+        <article key={rocket.id} className="flex items-center gap-5">
+          <img src={rocket.flickerImage} alt="rocket" className="w-72 rounded" />
+          <div className="flex flex-col gap-5">
+            <p className="text-xl font-bold">{rocket.rocketName}</p>
+            <p>{rocket.description}</p>
+            <button type="button" className="bg-blue-500 text-white px-5 py-2 w-32 rounded-md">Reserve</button>
+          </div>
+        </article>
       ))}
     </section>
   )
