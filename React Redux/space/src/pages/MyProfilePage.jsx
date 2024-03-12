29dd3258d076624e09964f9c108f2de22c1bc188
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-12 16:48:16 +0430

    Fetch rockets in MyProfilePage.jsx file

--- React Redux/space/src/pages/MyProfilePage.jsx
+++ React Redux/space/src/pages/MyProfilePage.jsx
@@ -1,5 +1,20 @@
-const MyProfilePage = () => (
-  <div>MyProfilePage</div>
-);
+import { useDispatch, useSelector } from "react-redux";
+import { getRockets, selectAllRockets } from "../redux/rockets/rocketsSlice";
+import { useEffect } from "react";
+
+const MyProfilePage = () => {
+  const rockets = useSelector(selectAllRockets);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets())
+  }, [dispatch]);
+
+  return (
+    <>
+    </>
+  );
+};
 
 export default MyProfilePage;
