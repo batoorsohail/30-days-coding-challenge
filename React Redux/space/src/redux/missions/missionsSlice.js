053db0c93b5ebd1c67e8c0398dbc59ce665bf330
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-12 21:31:46 +0430

    Add create missionsSlice

--- React Redux/space/src/redux/missions/missionsSlice.js
+++ React Redux/space/src/redux/missions/missionsSlice.js
@@ -5,6 +5,23 @@ const MISSIONS_URL = "https://api.spacexdata.com/v3/missions";
 
 const getMissions = createAsyncThunk("missions/getMissions", async() => {
   const response = await axios.get(MISSIONS_URL);
-  return response.data;
+  return response.data.map((mission) => ({
+    id: mission.mission_id,
+    missionName: mission.mission_name,
+    description: mission.description,
+    joinMission: false
+  }));
 })
 
+const initialState = {
+  missionsData: [],
+  status: "idle",
+  error: null
+}
+
+const missionsSlice = createSlice({
+  name: "missions",
+  initialState,
+  reducers: {
+  }
+})
