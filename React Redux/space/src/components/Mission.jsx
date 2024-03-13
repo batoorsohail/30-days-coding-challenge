f3fffe5737186ea8de837b9399d6e3d420e3a125
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-13 20:13:50 +0430

    Fix all Linter errors

--- React Redux/space/src/components/Mission.jsx
+++ React Redux/space/src/components/Mission.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-param-reassign */
-
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import {
@@ -37,7 +35,9 @@ const Mission = () => {
             <th className="border py-2">
               Status
             </th>
-            <th className="border py-2" />
+            <th className="border py-2">
+              Leave/Join
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -58,8 +58,8 @@ const Mission = () => {
               </td>
               <td className="border px-2">
                 {mission.joinMission ? (
-                  <button onClick={() => handleLeaveMission(mission)} className="text-red-500 border border-red-500 rounded w-[110px] py-2">Leave Mission</button>)
-                  : (<button onClick={() => handleJoinMission(mission)} className="text-slate-400 border border-slate-400 rounded w-[110px] py-2">Join Mission</button>)}
+                  <button type="button" onClick={() => handleLeaveMission(mission)} className="text-red-500 border border-red-500 rounded w-[110px] py-2">Leave Mission</button>)
+                  : (<button type="button" onClick={() => handleJoinMission(mission)} className="text-slate-400 border border-slate-400 rounded w-[110px] py-2">Join Mission</button>)}
               </td>
             </tr>
           ))}
