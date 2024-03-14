71de9cbe405c47380afd70f4dc6c47c3f0206d7b
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-14 21:21:40 +0430

    Fix Linter errors

--- React Redux/space/src/pages/MyProfilePage.jsx
+++ React Redux/space/src/pages/MyProfilePage.jsx
@@ -7,34 +7,34 @@ const MyProfilePage = () => {
   const missions = useSelector(selectAllMissions);
 
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
-  const joinedMissions = missions.filter((mission) => mission.joinMission)
+  const joinedMissions = missions.filter((mission) => mission.joinMission);
 
   return (
-    <section className='flex gap-10 px-10 justify-center'>
-      <article className='w-1/3'>
-        <h2 className='text-2xl font-bold'>My Rockets</h2>
+    <section className="flex gap-10 px-10 justify-center">
+      <article className="w-1/3">
+        <h2 className="text-2xl font-bold">My Rockets</h2>
         {reservedRockets.length > 0 ? (
           reservedRockets.map((rocket) => (
-            <div key={rocket.id} className='p-5 border'>
+            <div key={rocket.id} className="p-5 border">
               <p>{rocket.rocketName}</p>
             </div>
           ))
         ) : (
-          <div className='p-5 border'>
+          <div className="p-5 border">
             <p>No rocket reserved</p>
           </div>
         )}
       </article>
-      <article className='w-1/3'>
-        <h2 className='text-2xl font-bold'>My Missions</h2>
+      <article className="w-1/3">
+        <h2 className="text-2xl font-bold">My Missions</h2>
         {joinedMissions.length > 0 ? (
           joinedMissions.map((mission) => (
-            <div key={mission.id} className='p-5 border'>
+            <div key={mission.id} className="p-5 border">
               <p>{mission.missionName}</p>
             </div>
           ))
         ) : (
-          <div className='p-5 border'>
+          <div className="p-5 border">
             <p>No mission joined</p>
           </div>
         )}
