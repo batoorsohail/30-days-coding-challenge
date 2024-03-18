27447cde4b726661bd9ca08a3f22cbfb9fd78780
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-18 22:13:36 +0430

    add get coincoins asyncthunk function

--- React Redux/final-project/src/redux/coins/coinsSlice.js
+++ React Redux/final-project/src/redux/coins/coinsSlice.js
@@ -1,10 +1,23 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   coins: [],
   status: 'idle',
   error: null,
 }
+
+const COINS_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
+
+const getCoins = createAsyncThunk('coins/getCoins', async() => {
+  const response = await axios.get(COINS_URL);
+  return response.data.map((coin) => ({
+    coinId: coin.id,
+    coinName: coin.name,
+    coinImage: coin.image,
+    coinPrice: coin.current_price
+  }));
+})
 
 const coinsSlice = createSlice({
   initialState,
