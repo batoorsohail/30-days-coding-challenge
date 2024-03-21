6a8c0331d0096ce76d7261c887f47143bff99968
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-21 14:15:25 +0430

    Add CoinDetailsPage.jsx styling

--- React Redux/final-project/src/pages/CoinDetailsPage.jsx
+++ React Redux/final-project/src/pages/CoinDetailsPage.jsx
@@ -9,20 +9,22 @@ const CoinDetailsPage = () => {
   const cryptoCoin = coins.find((coin) => coin.coinId === coinId);
 
   return (
-    <section>
-      <div>
-        <img src={cryptoCoin.coinImage} alt={cryptoCoin.coinName} />
+    <section className="bg-pink-500 h-screen">
+      <div className="flex justify-between items-center px-10 py-5">
+        <img  className="w-32" src={cryptoCoin.coinImage} alt={cryptoCoin.coinName} />
         <div>
-          <h2>{cryptoCoin.coinName}</h2>
-          <p>{cryptoCoin.coinPrice}</p>
+          <h2 className="text-white text-3xl font-bold">{cryptoCoin.coinName}</h2>
+          <p className="text-white text-xl font-bold">{cryptoCoin.coinPrice}</p>
         </div>
       </div>
-      <section>
-        <div>
-          <p>{cryptoCoin.coinRank}</p>
+      <section className="coin-section">
+        <div className="flex items-center justify-between p-5 bg-pink-400">
+          <p className="text-white text-lg font-bold">Coin Rank:</p>
+          <p className="text-white text-lg font-bold">{cryptoCoin.coinRank}</p>
         </div>
-        <div>
-          <p>{cryptoCoin.coinPriceChange}</p>
+        <div className="flex items-center justify-between p-5 bg-pink-400">
+          <p className="text-white text-lg font-bold">Coin Price Change:</p>
+          <p className="text-white text-lg font-bold">{cryptoCoin.coinPriceChange}</p>
         </div>
       </section>
     </section>
