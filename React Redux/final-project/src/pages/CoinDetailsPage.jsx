f8b71ed4aeafc06e20cbf823cca2c3a1d29f7e5e
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-21 14:04:37 +0430

    Update CoinDetailsPage.jsx file

--- React Redux/final-project/src/pages/CoinDetailsPage.jsx
+++ React Redux/final-project/src/pages/CoinDetailsPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { useSelector } from "react-redux";
+import { selectAllCoins } from "../redux/coins/coinsSlice";
+import { useParams } from "react-router-dom";
 
-const CoinDetailsPage = () => (
-  <div>CoinDetailsPage</div>
-);
+const CoinDetailsPage = () => {
+  const coins = useSelector(selectAllCoins);
+
+  return(
+    <section>
+          <div>
+            <img src={cryptoCoin.coinImage} alt={cryptoCoin.coinName} />
+            <div>
+              <h2>{cryptoCoin.coinName}</h2>
+              <p>{cryptoCoin.coinPrice}</p>
+            </div>
+          </div>
+          <section>
+            <div>
+              <p>{cryptoCoin.coinRank}</p>
+            </div>
+            <div>
+              <p>{cryptoCoin.coinPriceChange}</p>
+            </div>
+          </section>
+    </section>
+  )
+};
 
 export default CoinDetailsPage;
