b9da7ee5b39b78ec32ad6a5b2f3d07f319129a1e
Sohail Batoor <sohailshahidi1435@gmail.com>
2025-08-24 16:24:23 +0800

    Add project popup modal

--- JavaScript/microverse/portfolio/index.js
+++ JavaScript/microverse/portfolio/index.js
@@ -15,12 +15,14 @@ const projects = [
   {
     title: 'Project One',
     description: 'A brief description of Project One.',
+    languages: 'React, Express, MongoDB',
     image: './work1.JPG',
     link: '#',
   },
   {
     title: 'Project Two',
     description: 'A brief description of Project Two.',
+    languages: 'React, Express, MongoDB',
     image: './work2.JPG',
     link: '#',
   }
@@ -28,7 +30,45 @@ const projects = [
 
 const projectsContainer = document.querySelector('.projects-container');
 
-const createWorks = project => {
+const projectDetailsSection = document.querySelector('.project-details-section');
+
+const projectDetails = (project) => {
+  projectDetailsSection.innerHTML = '';
+
+  const projectCard = document.createElement('div');
+  projectCard.classList.add('project-card-details');
+
+  const projectImage = document.createElement('img');
+  projectImage.classList.add('project-image');
+  projectImage.src = project.image;
+  projectImage.alt = project.title;
+  projectCard.appendChild(projectImage);
+
+  const projectTitle = document.createElement('h3');
+  projectTitle.classList.add('project-title');
+  projectTitle.textContent = project.title;
+  projectCard.appendChild(projectTitle);
+
+  const projectDescription = document.createElement('p');
+  projectDescription.classList.add('project-description');
+  projectDescription.textContent = project.description;
+  projectCard.appendChild(projectDescription);
+
+  const projectLanguages = document.createElement('p');
+  projectLanguages.classList.add('project-languages');
+  projectLanguages.textContent = project.languages;
+  projectCard.appendChild(projectLanguages);
+
+  const projectLink = document.createElement('a');
+  projectLink.classList.add('project-link');
+  projectLink.href = project.link;
+  projectLink.textContent = "View Project";
+  projectCard.appendChild(projectLink);
+
+  projectDetailsSection.appendChild(projectCard);
+}
+
+const createWorks = (project) => {
   const projectCard = document.createElement('div');
   projectCard.classList.add('project-card');
 
@@ -53,6 +93,13 @@ const createWorks = project => {
   projectLink.href = project.link;
   projectLink.textContent = "View Project";
   projectCard.appendChild(projectLink);
+
+  const projectDetailsBtn = document.createElement('button');
+  projectDetailsBtn.classList.add('project-details-btn');
+  projectDetailsBtn.textContent = "Project Details";
+  projectCard.appendChild(projectDetailsBtn);
+
+  projectDetailsBtn.addEventListener('click', () => projectDetails(project))
 
   projectsContainer.appendChild(projectCard);
 };
