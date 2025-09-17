73e40ed42f6feacb8372a5e3ea6471eb921bd05c
Sohail Batoor <sohailshahidi1435@gmail.com>
2025-09-17 14:38:04 +0800

    Add remove book listner

--- JavaScript/microverse/Awesome Books/main.js
+++ JavaScript/microverse/Awesome Books/main.js
@@ -3,18 +3,7 @@ const inputTitle = document.querySelector('.input-title');
 const inputAuthor = document.querySelector('.input-author');
 const submitBtn = document.querySelector('.submit-btn');
 
-const addedBooks = [
-  {
-    title: "Can't hurt me",
-    author: "David Goggins",
-  },
-  {
-    title: "Atomic Habits",
-    author: "James Clear"
-  }
-];
-
-const books = JSON.parse(localStorage.getItem('books'));
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
 const addNewBook = () => {
   const title = inputTitle.value;
@@ -28,28 +17,49 @@ const addNewBook = () => {
 
   inputTitle.value = "";
   inputAuthor.value = "";
+
+  renderBooks();
 }
 
 submitBtn.addEventListener('click', addNewBook);
 
-books.forEach(book => {
-  const bookContainer = document.createElement('div');
-  bookContainer.classList.add('book-container');
+const renderBooks = () => {
+  booksSection.innerHTML = "";
 
-  const bookName = document.createElement('p');
-  bookName.textContent += book.title;
-  bookName.classList.add('book-name');
-  bookContainer.appendChild(bookName);
+  if (books.length === 0) {
+    const text = document.createElement('p');
+    text.textContent = "No books added";
+    booksSection.appendChild(text);
+  } else {
+    books.forEach((book, index) => {
+      const bookContainer = document.createElement('div');
+      bookContainer.classList.add('book-container');
 
-  const authorName = document.createElement('p');
-  authorName.textContent += book.author;
-  authorName.classList.add('author-name');
-  bookContainer.appendChild(authorName);
+      const bookName = document.createElement('p');
+      bookName.textContent += book.title;
+      bookName.classList.add('book-name');
+      bookContainer.appendChild(bookName);
 
-  const removeBtn = document.createElement('button');
-  removeBtn.textContent += "Remove";
-  removeBtn.classList.add('remove-btn');
-  bookContainer.appendChild(removeBtn);
+      const authorName = document.createElement('p');
+      authorName.textContent += book.author;
+      authorName.classList.add('author-name');
+      bookContainer.appendChild(authorName);
 
-  booksSection.appendChild(bookContainer);
-});
+      const removeBtn = document.createElement('button');
+      removeBtn.textContent += "Remove";
+      removeBtn.classList.add('remove-btn');
+
+      removeBtn.addEventListener('click', () => {
+        books.splice(index, 1);
+        localStorage.setItem('books', JSON.stringify(books));
+        renderBooks();
+      });
+
+      bookContainer.appendChild(removeBtn);
+
+      booksSection.appendChild(bookContainer);
+    });
+  }
+};
+
+renderBooks();
