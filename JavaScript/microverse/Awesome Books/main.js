fa6ced2fbee19aa98c457c55b2e6bede5e7fef93
Sohail Batoor <sohailshahidi1435@gmail.com>
2025-09-22 15:25:35 +0800

    Refactor code to use JS Classes

--- JavaScript/microverse/Awesome Books/main.js
+++ JavaScript/microverse/Awesome Books/main.js
@@ -3,63 +3,74 @@ const inputTitle = document.querySelector('.input-title');
 const inputAuthor = document.querySelector('.input-author');
 const submitBtn = document.querySelector('.submit-btn');
 
-const books = JSON.parse(localStorage.getItem('books')) || [];
+class Books {
+  constructor(title, author) {
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.title = title;
+    this.author = author
+  }
 
-const addNewBook = () => {
-  const title = inputTitle.value;
-  const author = inputAuthor.value;
+  renderBooks() {
+    booksSection.innerHTML = "";
 
-  if (!title || !author) return;
+    if (this.books.length === 0) {
+      const text = document.createElement('p');
+      text.textContent = "No books added";
+      booksSection.appendChild(text);
+    } else {
+      this.books.forEach((book, index) => {
+        const bookContainer = document.createElement('div');
+        bookContainer.classList.add('book-container');
 
-  books.push({ title, author });
-  localStorage.setItem('books', JSON.stringify(books));
-  console.log(books);
+        const bookName = document.createElement('p');
+        bookName.textContent += book.title;
+        bookName.classList.add('book-name');
+        bookContainer.appendChild(bookName);
 
-  inputTitle.value = "";
-  inputAuthor.value = "";
+        const authorName = document.createElement('p');
+        authorName.textContent += book.author;
+        authorName.classList.add('author-name');
+        bookContainer.appendChild(authorName);
 
-  renderBooks();
-}
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent += "Remove";
+        removeBtn.classList.add('remove-btn');
 
-submitBtn.addEventListener('click', addNewBook);
+        removeBtn.addEventListener('click', () => {
+          this.removeBook(index);
+        });
 
-const renderBooks = () => {
-  booksSection.innerHTML = "";
+        bookContainer.appendChild(removeBtn);
 
-  if (books.length === 0) {
-    const text = document.createElement('p');
-    text.textContent = "No books added";
-    booksSection.appendChild(text);
-  } else {
-    books.forEach((book, index) => {
-      const bookContainer = document.createElement('div');
-      bookContainer.classList.add('book-container');
-
-      const bookName = document.createElement('p');
-      bookName.textContent += book.title;
-      bookName.classList.add('book-name');
-      bookContainer.appendChild(bookName);
-
-      const authorName = document.createElement('p');
-      authorName.textContent += book.author;
-      authorName.classList.add('author-name');
-      bookContainer.appendChild(authorName);
-
-      const removeBtn = document.createElement('button');
-      removeBtn.textContent += "Remove";
-      removeBtn.classList.add('remove-btn');
-
-      removeBtn.addEventListener('click', () => {
-        books.splice(index, 1);
-        localStorage.setItem('books', JSON.stringify(books));
-        renderBooks();
+        booksSection.appendChild(bookContainer);
       });
+    }
+  }
 
-      bookContainer.appendChild(removeBtn);
+  addBook() {
+    const title = inputTitle.value;
+    const author = inputAuthor.value;
 
-      booksSection.appendChild(bookContainer);
-    });
+    if (!title || !author) return;
+
+    this.books.push({ title, author });
+    localStorage.setItem('books', JSON.stringify(this.books));
+
+    inputTitle.value = "";
+    inputAuthor.value = "";
+
+    this.renderBooks();
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.renderBooks();
   }
 };
 
-renderBooks();
+const book = new Books(inputTitle, inputAuthor);
+
+submitBtn.addEventListener('click', () => book.addBook());
+
+book.renderBooks();
