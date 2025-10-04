3a61d1643ce3f7515d9a39e4c804b02c996fd490
Sohail Batoor <sohailshahidi1435@gmail.com>
2025-10-04 12:40:57 +0800

    Display todos

--- JavaScript/microverse/todo-list/src/index.js
+++ JavaScript/microverse/todo-list/src/index.js
@@ -1,14 +1,45 @@
-import _ from 'lodash';
-import './style.css';
+const toDosContainer = document.querySelector('.todos-container');
 
-function component() {
-  const element = document.createElement('div');
+const toDos = [
+  {
+    description: "Read 10 pages of a book",
+    completed: false,
+    index: 1
+  },
+  {
+    description: "Exercising for 1 hour",
+    completed: false,
+    index: 2
+  },
+  {
+    description: "Coding for 1 hour",
+    completed: false,
+    index: 3
+  },
+  {
+    description: "Study for 1 hour",
+    completed: false,
+    index: 4
+  }
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const displayTodos = () => {
+  toDos.map(todo => {
+    const todoList = document.createElement('li');
+    todoList.classList.add('todo-list');
 
-  return element;
-}
+    const todoComplete = document.createElement('select');
+    todoComplete.textContent = todo.completed;
+    todoList.classList.add('todo-complete');
+    todoList.appendChild(todoComplete);
 
-document.body.appendChild(component());
+    const todoDesc = document.createElement('p');
+    todoDesc.textContent = todo.description;
+    todoDesc.classList.add('todo-desc');
+    todoList.appendChild(todoDesc);
+
+    toDosContainer.appendChild(todoList);
+  })
+};
+
+displayTodos();
