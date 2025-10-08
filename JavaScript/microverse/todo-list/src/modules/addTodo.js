6d94412f290427d991cbcd6d672fd9885225797e
Sohail Batoor <sohailshahidi1435@gmail.com>
2025-10-08 23:53:20 +0800

    Fix clearCompletedTodos file bug

--- JavaScript/microverse/todo-list/src/modules/addTodo.js
+++ JavaScript/microverse/todo-list/src/modules/addTodo.js
@@ -1,8 +1,13 @@
 const addTodo = (toDos, todoInput) => {
   const todoInputValue = todoInput.value;
-  toDos.push({ description: todoInputValue, completed: false });
-  localStorage.setItem('todos', JSON.stringify(toDos));
-  todoInput.value = '';
+  
+  if (todoInputValue === '') {
+    throw new Error('Input should have a value');
+  } else {
+    toDos.push({ description: todoInputValue, completed: false });
+    localStorage.setItem('todos', JSON.stringify(toDos));
+    todoInput.value = '';
+  }
 };
 
 export default addTodo;
--- JavaScript/microverse/todo-list/src/modules/clearCompletedTodos.js
+++ JavaScript/microverse/todo-list/src/modules/clearCompletedTodos.js
@@ -1,5 +1,7 @@
 const clearCompletedTodos = (toDos, displayTodos) => {
-  toDos = toDos.filter(todo => !todo.completed);
+  const updatedTodos = toDos.filter(todo => !todo.completed);
+  toDos.length = 0;
+  toDos.push(...updatedTodos);
   localStorage.setItem('todos', JSON.stringify(toDos));
   displayTodos(toDos);
 }
